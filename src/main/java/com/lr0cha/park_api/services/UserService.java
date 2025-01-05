4a876cb097907211f0a6b8bc2554d2d96a57eb57
Lucas Rocha <ldrocha16@gmail.com>
2025-01-05 20:23:57 -0300

    Implementação dos endpoints GET,POST,PATCH da entidade User

--- src/main/java/com/lr0cha/park_api/services/UserService.java
+++ src/main/java/com/lr0cha/park_api/services/UserService.java
@@ -1,9 +1,14 @@
 package com.lr0cha.park_api.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.lr0cha.park_api.entities.User;
 import com.lr0cha.park_api.repositories.UserRepository;
+
 
 
 @Service //reconhece como um bean gerenciado
@@ -11,4 +16,27 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	@Transactional(readOnly = true)
+	public User findById(Long id) {
+		return repository.findById(id).get();
+	}
+	
+	@Transactional
+	public User insert(User user) {
+		return repository.save(user);
+	}
+	
+	@Transactional
+	public User updatePassword(Long id, String password) {
+		User user = findById(id);
+		user.setPassword(password);
+		return user;
+	}
+
 }
--- src/main/java/com/lr0cha/park_api/web/controllers/UserController.java
+++ src/main/java/com/lr0cha/park_api/web/controllers/UserController.java
@@ -1,9 +1,19 @@
 package com.lr0cha.park_api.web.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lr0cha.park_api.entities.User;
 import com.lr0cha.park_api.services.UserService;
 
 @RestController
@@ -11,4 +21,29 @@ import com.lr0cha.park_api.services.UserService;
 public class UserController {
 	@Autowired
 	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity <List<User>> findAll(){
+		List<User> user = service.findAll();
+		return ResponseEntity.ok().body(user);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user){
+		user = service.insert(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(user);
+	}
+	
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<User> updatePassword(@PathVariable Long id, @RequestBody User user){
+		user = service.updatePassword(id, user.getPassword());
+		return ResponseEntity.ok().body(user);
+	}	
 }
