7b09580030e53362f472ffb5c8fef5cc6757ccd2
Lucas Rocha <ldrocha16@gmail.com>
2025-01-05 18:40:53 -0300

    Criação entidade User

---
+++ src/main/java/com/lr0cha/park_api/entities/User.java
@@ -0,0 +1,72 @@
+package com.lr0cha.park_api.entities;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import com.lr0cha.park_api.entities.enums.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "tb_users")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true, length = 100)
+	private String username;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 20)
+	private Role role;
+	
+	@Column(name = "creation_date")
+	private LocalDateTime creationDate;
+	
+	@Column(name = "modification_date")
+	private LocalDateTime modificationDate;
+	
+	@Column(name = "created_by")
+	private String createdBy;
+	
+	@Column(name = "modify_by")
+	private String modifyBy;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
---
+++ src/main/java/com/lr0cha/park_api/entities/enums/Role.java
@@ -0,0 +1,6 @@
+package com.lr0cha.park_api.entities.enums;
+
+public enum Role {
+	ADMINISTRATOR,
+	CLIENT
+}
